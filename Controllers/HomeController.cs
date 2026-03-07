f80be936ddccf607897aeabb757fc281baa485f5
davronesonboev-tech <davronesonboev@gmail.com>
2026-03-07 14:11:33 +0500

    Phase 0: Hello World, ASP.NET Core MVC auth setup, and connected to PostgreSQL on Railway

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using InventoryApp.Models;
+using InventoryApp.Data;
+
+namespace InventoryApp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public HomeController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        bool dbConnected = false;
+        try
+        {
+            dbConnected = _context.Database.CanConnect();
+        }
+        catch (Exception)
+        {
+            dbConnected = false;
+        }
+
+        ViewBag.DbConnected = dbConnected;
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using InventoryApp.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Parse DATABASE_PUBLIC_URL from environment
+var dbUrl = Environment.GetEnvironmentVariable("DATABASE_PUBLIC_URL") 
+            ?? builder.Configuration.GetConnectionString("DefaultConnection");
+
+string connectionString = dbUrl ?? "";
+if (connectionString.StartsWith("postgres://") || connectionString.StartsWith("postgresql://"))
+{
+    var uri = new Uri(connectionString);
+    var userInfo = uri.UserInfo.Split(':');
+    var db = uri.LocalPath.TrimStart('/');
+    connectionString = $"Host={uri.Host};Port={uri.Port};Database={db};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Prefer;Trust Server Certificate=true;";
+}
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Apply migrations automatically on startup
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+app.MapRazorPages();
+
+app.Run();
